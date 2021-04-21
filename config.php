fdebfd08aa63740565fb3d12d18341b2917b51d6
Carlos Alzate <carlosalzatep@gmail.com>
2021-04-21 09:27:22 -0500

    Push

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Global Vars
+ */
+
+ const ENDPOINT = 'https://api.supermetrics.com/assignment/';
+ const CLIENT_ID = 'ju16a6m81mhid5ue1z3v2g0uh';
+ const EMAIL = 'carlosalzatep@gmail.com';
+ const NAME = 'Carlos Alzate';
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+include_once('config.php');
+include_once('api.php');
+
+echo "Hello World!!!";
